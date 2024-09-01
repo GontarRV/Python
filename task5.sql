3f1582c231e4137b3cdf8e41f5af9ead6f61511a
GontarRV <150484364+GontarRV@users.noreply.github.com>
2024-09-01 22:40:27 +0300

    add task 5 SQL

---
+++ task5.sql
@@ -0,0 +1,17 @@
+Задания 5.4.
+5.4.1.
+SELECT * FROM Employees 
+ORDER BY BirthDate DESC, Country
+
+5.4.2.
+SELECT * FROM Employees
+WHERE Region IS NOT NULL
+ORDER BY BirthDate DESC, Country
+
+5.4.3.
+SELECT AVG(UnitPrice), MIN(UnitPrice), MAX(UnitPrice) FROM [Order Details]
+# 26.2185; 2.00; 263.50 
+
+5.4.4.
+SELECT COUNT(DISTINCT City) FROM Customers
+# 69
