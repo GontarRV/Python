1510b53336a27dcb86c6e219883cbfa0f1cdd08c
GontarRV <150484364+GontarRV@users.noreply.github.com>
2024-09-09 22:48:51 +0300

    add task 9 SQL

---
+++ task9.sql
@@ -0,0 +1,13 @@
+Задания 9.4.
+Задание 9.4.1
+SELECT t1.CustomerID, t2.CustomerID
+FROM Customers t1, Customers t2
+WHERE t1.Region IS NULL AND t2.Region IS NULL
+
+Задание 9.4.2
+SELECT * FROM Orders
+WHERE CustomerID = ANY (SELECT CustomerID FROM Customers WHERE Region IS NOT NULL)
+
+Задание 9.4.3
+SELECT * FROM Orders
+WHERE Freight > ANY (SELECT Products.UnitPrice FROM Products)
