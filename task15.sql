5f06a8e820204245b87bf0b057abb4d3bf74cf3a
GontarRV <150484364+GontarRV@users.noreply.github.com>
2024-09-21 01:28:51 +0300

    add task 15 SQL

---
+++ task15.sql
@@ -0,0 +1,16 @@
+Задание 15.7.
+ALTER TABLE Region ADD CONSTRAINT PK_Region_RegionID PRIMARY KEY (RegionID);
+
+CREATE TABLE Territories (
+     TerritoryID int NOT NULL,
+     TerritoryDescription nchar(50) NOT NULL,
+     RegionID int NOT NULL,
+     CONSTRAINT PK_Territories_TerritoryID PRIMARY KEY (TerritoryID),
+     CONSTRAINT FK_Territories_Region FOREIGN KEY (RegionID)
+         REFERENCES Region (RegionID) );
+
+INSERT INTO Region (RegionID, RegionDescription)
+VALUES (1, 'Moscow'), (2, 'Piter');
+
+INSERT INTO Territories (TerritoryID, TerritoryDescription, RegionID)
+VALUES (1, 'Kremlin', 1), (2, 'Big Theatre', 1), (3, 'Kunstkammer', 2);
