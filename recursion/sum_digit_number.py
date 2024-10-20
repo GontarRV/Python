aa1b93813bf70ae194e7ce53849fe23c39a1ccaf
GontarRV <roman.gontar@bk.ru>
2024-10-20 23:05:14 +0300

    update recursion task2

--- recursion/sum_digit_number.py
+++ recursion/sum_digit_number.py
@@ -1,4 +1,5 @@
 def sum_digit_number(N: int) -> int:
-    if len(str(N)) == 1:
+    if (N // 10) < 1:
         return N
-    return int(str(N)[0]) + sum_digit_number(int(str(N)[1:]))
+    return (N % 10) + sum_digit_number(N // 10)
+
