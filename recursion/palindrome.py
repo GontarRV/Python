c14c8b0864281f38aac893342aaadf546bd98359
GontarRV <150484364+GontarRV@users.noreply.github.com>
2024-10-25 08:32:04 +0300

    Update task4

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -1,8 +1,15 @@
-def palindrome(string: str, move:int = 0) -> bool:
+def palindrome_recursive(string: str, move:int) -> bool:
     if len(string) // 2 <= move:
         return True
 
     if string[move] != string[-1 - move]:
         return False
-        
-    return palindrome(string, move + 1)
+
+    return palindrome_recursive(string, move + 1)
+
+
+def palindrome(string: str):
+    if len(string) == 0:
+        return True
+
+    return palindrome_recursive(string, 0)
