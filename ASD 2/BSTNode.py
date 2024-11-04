9d70f3371c51fa5031a310979e4c27251ae465b6
GontarRV <roman.gontar@bk.ru>
2024-11-04 19:43:12 +0300

    add ASD task 2 and test

---
+++ ASD 2/BSTNode.py
@@ -0,0 +1,136 @@
+class BSTNode:
+    def __init__(self, key, val, parent):
+        self.NodeKey = key
+        self.NodeValue = val
+        self.Parent = parent
+        self.LeftChild = None
+        self.RightChild = None
+        
+
+class BSTFind:
+    def __init__(self):
+        self.Node = None
+        self.NodeHasKey = False
+        self.ToLeft = False
+
+
+class BST:
+    def __init__(self, node):
+        self.Root = node
+        
+    def FindNodeByKey(self, key):
+        if self.Root is None:
+            return BSTFind()
+        
+        return self._FindNodeByKeyRecursive(key, self.Root)
+
+    def AddKeyValue(self, key, val):
+        if self.Root is None:
+            self.Root = BSTNode(key, val, parent=None)
+            return True
+        return self._add_key_value(self.Root, key, val)
+  
+    def FinMinMax(self, FromNode, FindMax: bool):
+        if (FindMax and FromNode.RightChild is None) or (not FindMax and FromNode.LeftChild is None):
+            return FromNode
+        elif FindMax:
+            return self.FinMinMax(FromNode.RightChild, FindMax)
+        else:
+            return self.FinMinMax(FromNode.LeftChild, FindMax)
+	
+    def DeleteNodeByKey(self, key):
+
+        findResult = self.FindNodeByKey(key)
+        
+        if not findResult.NodeHasKey:
+            return False
+        node = findResult.Node
+
+        if node.LeftChild is None or node.RightChild is None:
+            self._DeleteNodeWithMaxOneChild(node)
+            return True
+        
+        parent = findResult.Node.Parent
+        successor = self.FinMinMax(node.RightChild, False)
+        self._DeleteNodeWithMaxOneChild(successor)
+        successor.LeftChild = node.LeftChild
+        successor.RightChild = node.RightChild
+        node.LeftChild.Parent = successor
+
+        if node.RightChild is not None:
+            node.RightChild.Parent = successor
+
+        successor.Parent = parent
+
+        if parent is None:
+            self.Root = successor
+        elif successor.NodeKey < parent.NodeKey:
+            parent.LeftChild = successor
+        else:
+            parent.RightChild = successor
+        
+        return True
+
+    def Count(self):
+        self._counter = 0
+
+        if self.Root is not None:
+            self._CountNodes(self.Root)
+
+        return self._counter
+
+    def _FindNodeByKeyRecursive(self, key, currentNode):
+        if key < currentNode.NodeKey and currentNode.LeftChild is not None:
+            return self._FindNodeByKeyRecursive(key, currentNode.LeftChild)
+        elif key > currentNode.NodeKey and currentNode.RightChild is not None:
+            return self._FindNodeByKeyRecursive(key, currentNode.RightChild)
+        
+        result = BSTFind()
+        result.Node = currentNode
+        
+        if key == currentNode.NodeKey:
+             result.NodeHasKey = True
+        elif key < currentNode.NodeKey:
+            result.ToLeft = True
+        
+        return result
+
+    def _add_key_value(self, Node, key, value):
+        if Node.NodeKey == key:
+            return False
+        if (Node.NodeKey < key) and (Node.RightChild is None):
+            Node.RightChild = BSTNode(key, value, Node)
+            return True
+        if (Node.NodeKey > key) and (Node.LeftChild is None):
+            Node.LeftChild = BSTNode(key, value, Node)
+            return True
+        if Node.NodeKey < key:
+            return self._add_key_value(Node.RightChild, key, value) 
+        return self._add_key_value(Node.LeftChild, key, value)
+
+    def _DeleteNodeWithMaxOneChild(self, node):
+        parent = node.Parent
+        successor = None
+
+        if node.RightChild is not None:
+            successor = node.RightChild
+        elif node.LeftChild is not None:
+            successor = node.LeftChild
+
+        if parent is None:
+            self.Root = successor
+        elif node.NodeKey < parent.NodeKey:
+            parent.LeftChild = successor
+        else:
+            parent.RightChild = successor
+
+        if successor is not None:
+            successor.Parent = parent
+
+    def _CountNodes(self, currentNode):
+        self._counter += 1
+        
+        if currentNode.LeftChild is not None:
+            self._CountNodes(currentNode.LeftChild)
+        if currentNode.RightChild is not None:
+            self._CountNodes(currentNode.RightChild)
---
+++ ASD 2/testsBSTNode.py
@@ -0,0 +1,239 @@
+import unittest
+import random
+from BSTNode import BSTNode, BSTFind, BST
+
+
+class BSTTests(unittest.TestCase):
+
+	def getTestTree(self):
+		nodes = []
+
+		for i in range(15):
+			nodes.append(BSTNode(i, i, None))
+
+		nodes[1].LeftChild = nodes[0]
+		nodes[1].RightChild = nodes[2]
+		nodes[0].Parent = nodes[1]
+		nodes[2].Parent = nodes[1]
+
+		nodes[5].LeftChild = nodes[4]
+		nodes[5].RightChild = nodes[6]
+		nodes[4].Parent = nodes[5]
+		nodes[6].Parent = nodes[5]
+
+		nodes[3].LeftChild = nodes[1]
+		nodes[3].RightChild = nodes[5]
+		nodes[1].Parent = nodes[3]
+		nodes[5].Parent = nodes[3]
+
+		nodes[9].LeftChild = nodes[8]
+		nodes[9].RightChild = nodes[10]
+		nodes[8].Parent = nodes[9]
+		nodes[10].Parent = nodes[9]
+
+		nodes[13].LeftChild = nodes[12]
+		nodes[13].RightChild = nodes[14]
+		nodes[12].Parent = nodes[13]
+		nodes[14].Parent = nodes[13]
+
+		nodes[11].LeftChild = nodes[9]
+		nodes[11].RightChild = nodes[13]
+		nodes[9].Parent = nodes[11]
+		nodes[13].Parent = nodes[11]
+
+		nodes[7].LeftChild = nodes[3]
+		nodes[7].RightChild = nodes[11]
+		nodes[3].Parent = nodes[7]
+		nodes[11].Parent = nodes[7]
+
+		tree = BST(nodes[7])
+
+		return (tree, nodes)
+
+	def test_FindNodeByKeyEmpty(self):
+		tree = BST(None)
+		result = tree.FindNodeByKey(0)
+		self.assertTrue(result.Node is None)
+
+	def test_FindNodeByKeyOneNode(self):
+		testKey = 0
+		node = BSTNode(testKey, testKey, None)
+		tree = BST(node)
+		result = tree.FindNodeByKey(testKey)
+		self.assertTrue(result.Node is node)
+		self.assertTrue(result.NodeHasKey is True)
+
+	def test_FindNodeByKeyExisting(self):
+		(tree, nodes) = self.getTestTree()
+		result = tree.FindNodeByKey(3)
+		self.assertTrue(result.Node is nodes[3])
+		self.assertTrue(result.NodeHasKey is True)
+
+	def test_FindNodeByKeyNotExistingLeft(self):
+		(tree, nodes) = self.getTestTree()
+		result = tree.FindNodeByKey(-1)
+		self.assertTrue(result.Node is nodes[0])
+		self.assertTrue(result.NodeHasKey is False)
+		self.assertTrue(result.ToLeft is True)
+
+	def test_FindNodeByKeyNotExistingRight(self):
+		(tree, nodes) = self.getTestTree()
+		result = tree.FindNodeByKey(15)
+		self.assertTrue(result.Node is nodes[14])
+		self.assertTrue(result.NodeHasKey is False)
+		self.assertTrue(result.ToLeft is False)
+
+	def test_AddKeyValueToEmptyTree(self):
+		tree = BST(None)
+		testKey = 1
+		self.assertFalse(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(tree.AddKeyValue(testKey, testKey))
+		self.assertTrue(tree.FindNodeByKey(testKey).NodeHasKey)
+
+	def test_AddKeyValueToLeft(self):
+		(tree, nodes) = self.getTestTree()
+		testKey = -1
+		self.assertFalse(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(tree.AddKeyValue(testKey, testKey))
+		findResult = tree.FindNodeByKey(testKey)
+		self.assertTrue(findResult.NodeHasKey)
+		self.assertTrue(nodes[0].LeftChild is findResult.Node)
+
+	def test_AddKeyValueToRight(self):
+		(tree, nodes) = self.getTestTree()
+		testKey = 15
+		self.assertFalse(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(tree.AddKeyValue(testKey, testKey))
+		findResult = tree.FindNodeByKey(testKey)
+		self.assertTrue(findResult.NodeHasKey)
+		self.assertTrue(nodes[14].RightChild is findResult.Node)
+
+	def test_AddKeyValueAlreadyExisting(self):
+		(tree, nodes) = self.getTestTree()
+		testKey = 9
+		self.assertTrue(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertFalse(tree.AddKeyValue(testKey, testKey))
+
+	def test_FinMinMaxFromRootMin(self):
+		(tree, nodes) = self.getTestTree()
+		self.assertTrue(tree.FinMinMax(nodes[7], False) is nodes[0])
+
+	def test_FinMinMaxFromRootMax(self):
+		(tree, nodes) = self.getTestTree()
+		self.assertTrue(tree.FinMinMax(nodes[7], True) is nodes[14])
+
+	def test_FinMinMaxFromSubtreeMin(self):
+		(tree, nodes) = self.getTestTree()
+		self.assertTrue(tree.FinMinMax(nodes[11], False) is nodes[8])
+
+	def test_FinMinMaxFromSubtreeMax(self):
+		(tree, nodes) = self.getTestTree()
+		self.assertTrue(tree.FinMinMax(nodes[3], True) is nodes[6])
+
+	def test_DeleteNodeByKeyNotExisting(self):
+		(tree, nodes) = self.getTestTree()
+		self.assertFalse(tree.DeleteNodeByKey(-1))
+
+	def test_DeleteNodeByKeyEmpty(self):
+		tree = BST(None)
+		self.assertFalse(tree.DeleteNodeByKey(0))
+
+	def test_DeleteNodeByKeyOneNode(self):
+		testKey = 0
+		tree = BST(BSTNode(testKey, testKey, None))
+		self.assertTrue(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(tree.DeleteNodeByKey(testKey))
+		self.assertFalse(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(tree.Root is None)
+
+	def test_DeleteNodeByKeyWithNoChild(self):
+		(tree, nodes) = self.getTestTree()
+		testKey = 4
+		self.assertTrue(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(tree.DeleteNodeByKey(testKey))
+		self.assertFalse(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(nodes[5].LeftChild is None)
+
+	def test_DeleteNodeByKeyWithOnlyLeftChild(self):
+		(tree, nodes) = self.getTestTree()
+		testKey = 3
+		nodes[testKey].RightChild = None
+		self.assertTrue(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(tree.DeleteNodeByKey(testKey))
+		self.assertFalse(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(nodes[7].LeftChild is nodes[1])
+		self.assertTrue(nodes[1].Parent is nodes[7])
+
+	def test_DeleteNodeByKeyWithOnlyRightChild(self):
+		(tree, nodes) = self.getTestTree()
+		testKey = 11
+		nodes[testKey].LeftChild = None
+		self.assertTrue(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(tree.DeleteNodeByKey(testKey))
+		self.assertFalse(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(nodes[7].RightChild is nodes[13])
+		self.assertTrue(nodes[13].Parent is nodes[7])
+
+	def test_DeleteNodeByKeyWithBothChild(self):
+		(tree, nodes) = self.getTestTree()
+		testKey = 3
+		self.assertTrue(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(tree.DeleteNodeByKey(testKey))
+		self.assertFalse(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(nodes[7].LeftChild is nodes[4])
+		self.assertTrue(nodes[4].Parent is nodes[7])
+		self.assertTrue(nodes[4].LeftChild is nodes[1])
+		self.assertTrue(nodes[4].RightChild is nodes[5])
+		self.assertTrue(nodes[1].Parent is nodes[4])
+		self.assertTrue(nodes[5].Parent is nodes[4])
+		self.assertTrue(nodes[5].LeftChild is None)
+
+	def test_DeleteNodeByKeyRandom(self):
+		for _ in range(10000):
+			(tree, nodes) = self.getTestTree()
+			deletedKeys = []
+
+			while len(nodes):
+				testNode = random.choice(nodes)
+				testKey = testNode.NodeKey
+				deletedKeys.append(testKey)
+				self.assertTrue(tree.FindNodeByKey(testKey).NodeHasKey, deletedKeys)
+				nodes.remove(testNode)
+				self.assertTrue(tree.DeleteNodeByKey(testKey))
+				self.assertFalse(tree.FindNodeByKey(testKey).NodeHasKey)
+
+	def test_DeleteNodeByKeyCase_7_10(self):
+		(tree, nodes) = self.getTestTree()
+		nodes[9].LeftChild = None
+		keyToDelete = 7
+		problemKey = 10
+
+		self.assertTrue(tree.FindNodeByKey(keyToDelete).NodeHasKey)
+		self.assertTrue(tree.FindNodeByKey(problemKey).NodeHasKey)
+		self.assertTrue(tree.DeleteNodeByKey(keyToDelete))
+		self.assertFalse(tree.FindNodeByKey(keyToDelete).NodeHasKey)
+		self.assertTrue(tree.FindNodeByKey(problemKey).NodeHasKey)
+
+	def test_DeleteNodeByKeyLastNode(self):
+		(tree, nodes) = self.getTestTree()
+		testKey = 14
+		self.assertTrue(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(tree.DeleteNodeByKey(testKey))
+		self.assertFalse(tree.FindNodeByKey(testKey).NodeHasKey)
+		self.assertTrue(nodes[13].RightChild is None)
+
+	def test_CountEmpty(self):
+		tree = BST(None)
+		self.assertEqual(tree.Count(), 0)
+
+	def test_CountOneNode(self):
+		node = BSTNode(1, 1, None)
+		tree = BST(node)
+		self.assertEqual(tree.Count(), 1)
+
+	def test_Count(self):
+		(tree, nodes) = self.getTestTree()
+		self.assertEqual(tree.Count(), len(nodes))
+
+if __name__ == '__main__':
+	unittest.main()
