1c02fbec54d57b4afb8c0dbde323cb40c1bf9233
GontarRV <roman.gontar@bk.ru>
2024-11-08 14:57:20 +0300

    add ASD task 1 solution 1,3

---
+++ ASD 2/SimpleTree NodeTask.py
@@ -0,0 +1,28 @@
+# 1. Напишите метод, который перебирает 
+# всё дерево и прописывает каждому узлу его уровень.
+
+    def _set_level_nodes(self, node):
+        if node is self.Root:
+            node.Level = 0
+        for child in node.Children:
+            child.Level = child.Parent.Level + 1
+            self._set_level_nodes(child)
+
+    def SetLevelNodes(self):
+        self._set_level_nodes(self.Root)
+
+# 3.* (бонус +500) Добавьте метод проверки, 
+# симметрично ли дерево относительно своего корня.
+
+    def is_symmetric(tree: list) -> bool:
+        start = 0
+        end = 1
+        while start < len(tree):
+            stratum = tree[start:end]
+            if len(stratum) != (end - start) or stratum != list(reversed(stratum)):
+                return False
+            start = end
+            end = 2 * end  + 1
+        return True
+
+    
